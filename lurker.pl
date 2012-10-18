7991e7da5c55bc674ff7d89adfb908a3a60ff5dc
Matthew McPherrin <matthew@mcpherrin.ca>
2012-10-17 22:53:32 -0400

    Add /lurk and /unlurk commands

--- lurker.pl
+++ lurker.pl
@@ -1,5 +1,6 @@
 use strict;
 use warnings;
+use Data::Dumper;
 use 5.010;
 
 use vars qw($VERSION %IRSSI);
@@ -19,12 +20,44 @@ $VERSION = '1.00';
 
 Irssi::signal_add_first 'send text', 'lurkhandle';
 
+my $lurkchannels = {};
+$lurkchannels->{'#csc-test'} = "yes";
+
+sub lurk_cmd {
+  my($data, $server, $win_item) = @_;
+  if($win_item) {
+    $lurkchannels->{$win_item->{name}} = "yes";
+    $win_item->print("Now lurking in " . $win_item->{name});
+  } else {
+    print "No window.";
+  }
+}
+
+Irssi::command_bind('lurk', 'lurk_cmd');
+
+sub unlurk_cmd {
+  my($data, $server, $win_item) = @_;
+  if($win_item) {
+    delete $lurkchannels->{$win_item->{name}};
+    $win_item->print("Not lurking in " . $win_item->{name});
+  } else {
+    print "No window...";
+  }
+}
+
+Irssi::command_bind('unlurk', 'unlurk_cmd');
+
 sub lurkhandle {
   my($text, $server, $win_item) = @_;
   if($win_item && ($text =~ m/win\s*\d+/ || $text =~ m/^(f|j|k|\s|~|>|\.)+$/ )) {
     $win_item->print("Spamblock: " . $text);
     Irssi::signal_stop();
   } else {
-    Irssi::signal_continue($text, $server, $win_item);
+    if( $lurkchannels->{$win_item->{name}} ) {
+      $win_item->print("Lurkblock: " . $text);
+      Irssi::signal_stop();
+    } else {
+      Irssi::signal_continue($text, $server, $win_item);
+    }
   }
 }
